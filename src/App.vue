b941768d6b27d6a8e7f7a195dd67e9bf4d331399
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-10-20 17:36:44 +0400

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="wrapper">
+
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">GitHub Finder</div>
+            <!-- <ul class="navbar-list">
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</router-link>
+              </li>
+            </ul> -->
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <router-view></router-view>
+
+    <!-- <footer></footer> -->
+
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        { title: 'Home', url: '/' }
+      ]
     }
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar-link {
+  &.router-link-exact-active {
+    color: #5247e7;
   }
 }
 </style>
